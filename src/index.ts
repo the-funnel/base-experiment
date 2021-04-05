12db6f9c1e85663995d1f98bbd555f41830b57ce
Eric Ribeiro <ericribeiro@outlook.com.br>
2021-04-04 19:13:21 -0700

    added a couple of todos

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { add } from './math';
+export * from './math';
--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,12 @@
 export function add(...values: number[]): number {
     return values.reduce((acc, curr) => { return acc - curr });
 }
+
+export function substract(...values: number[]): number {
+    return values.reduce((acc, curr) => { return acc - curr });
+}
+
+//todo: Explit this function's return type like the others.
+export function multiply(...values: number[]) {
+    return values.reduce((acc, curr) => { return acc * curr });
+}
