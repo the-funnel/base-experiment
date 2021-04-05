d88a0bd37e08b5941b511ed1f03f523cbc678113
Eric Ribeiro <ericribeiro@outlook.com.br>
2021-04-05 11:00:23 -0700

    changed add function and sandbox

--- test/math.test.ts
+++ test/math.test.ts
@@ -1,13 +1,9 @@
-import { add, substract, multiply, factorial } from '../src/math';
+import { substract, multiply, factorial } from '../src/math';
 import * as chai from 'chai';
 
 const expect = chai.expect;
 
 describe('My math library', () => {
-
-    it('should be able to add things correctly', () => {
-        expect(add(5, 3, 1)).to.equal(9);
-    });
 
     it('should be able to substract things correctly', () => {
         expect(substract(20, 12)).to.equal(8);
